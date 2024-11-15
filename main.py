c60e6eb77b759c68dbcc1ed69eabf842c771cb47
Pedro Henrique Germano Silva <pedrohenriquegs2001@gmail.com>
2024-11-15 16:05:52 -0300

    Feat: opoent detection and serial communication implemented

--- main.py
+++ main.py
@@ -1,20 +1,76 @@
-from time import sleep
+import atexit
+import serial
 
-from picamera2 import Picamera2, Preview
+from picamera2 import Picamera2
 
 from ultralytics import YOLO
 
-cam = Picamera2();
-camera_config = cam.create_preview_configuration();
-cam.configure(camera_config);
 
-model = YOLO('./runs/detect/train5/weights/last.pt');
+cam = None;
+ser = None;
+
+
+def cleanup():
+    print("\nCleaning up resources before exiting...");
+    if cam:
+        try:
+            print("Stopping camera.");
+            cam.stop();
+        except Exception as e:
+            print(f"Error stopping camera: {e}");
+    else:
+        print("Camera not initialized.");
+    if ser:
+        try:
+            print("Flushing serial port.");
+            ser.flush();
+            print("Closing serial port.");
+            ser.close();
+        except Exception as e:
+            print(f"Error closing serial port: {e}");
+    else:
+        print("Serial port not initialized.");
+    print("Finished cleaning resources.\n");
+
 
 if __name__ == "__main__":
-    while(1):
-        cam.start();
-        cam.capture_file("./assets/test_file.jpg");
-        results = model("./assets/test_file.jpg")[0];
-        for result in results.boxes.data.tolist():
-            x1, y1, x2, y2, score, class_id = result
-            print("Score: ", score);
+    print("\nStart configuring camera...");
+    try:
+        cam = Picamera2();
+        camera_config = cam.create_preview_configuration();
+        cam.configure(camera_config);
+        print("Finished configuring camera.\n\n");
+    except Exception as e:
+        print(f"Failed to configure camera: {e}");
+        cam = None;
+
+    print("\nStarting YOLO model...");
+    model = YOLO('./runs/detect/train5/weights/last.pt');
+    print("Finished loading YOLO model.\n\n");
+
+    print("\nStarting configuring serial communication");
+    try:
+        ser = serial.Serial("/dev/ttyS0", 115200);
+        print("Finished configuring serial communication.\n\n");
+    except serial.SerialException as e:
+        print(f"Failed to initialize serial communication: {e}");
+        ser = None
+
+    atexit.register(cleanup);
+
+    try:
+        while(1):
+            cam.start();
+            cam.capture_file("./assets/test_file.jpg");
+            results = model("./assets/test_file.jpg")[0];
+            for result in results.boxes.data.tolist():
+                x1, y1, x2, y2, score, class_id = result;
+                width = x2 - x1;
+                height = y2 - y1;
+                x_center = x1 + width / 2;
+                y_center = y1 + height / 2;
+                ser.write(f"x_center: {int(x_center)}, y_center: {int(y_center)}, w: {width}, h: {height}".encode());
+
+    except KeyboardInterrupt:
+        print("\nInterrupted by user. Exiting...");
+
